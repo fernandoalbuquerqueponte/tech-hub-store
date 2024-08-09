637c785a092dd03fa9d0c71f8172019a4fd0eda5
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2024-08-09 08:55:32 -0300

    feat: add initial sheet menu content to component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,34 +1,21 @@
 "use client";
-import {
-  LogOutIcon,
-  MenuIcon,
-  ShoppingBagIcon,
-  ShoppingCartIcon,
-} from "lucide-react";
+import { MenuIcon, ShoppingCartIcon } from "lucide-react";
 
+import { Sheet, SheetContent, SheetTrigger } from "../_components/ui/sheet";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
+
 import Link from "next/link";
 
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../_components/ui/sheet";
-
-import { Avatar, AvatarFallback, AvatarImage } from "../_components/ui/avatar";
-import { signIn, useSession } from "next-auth/react";
+import MenuContent from "./menu-content";
 
 export default function Header() {
-  const { data: session } = useSession();
   return (
     <header>
       <Card>
         <CardContent className="flex p-5 h-24 items-center flex-row justify-between">
           <Link className="text-lg font-bold" href="/">
-            TecHub
+            TechHub
           </Link>
           <div className="flex flex-row gap-5">
             <Button className="w-8 h-8" variant="outline" size="icon">
@@ -42,39 +29,7 @@ export default function Header() {
                 </Button>
               </SheetTrigger>
               <SheetContent className="p-0">
-                <SheetHeader>
-                  <SheetTitle className="text-left px-5 py-6 text-2xsl">
-                    Menu
-                  </SheetTitle>
-                </SheetHeader>
-                <div className="flex items-center justify-between px-6 py-4">
-                  <div className="flex items-center gap-3">
-                    <Avatar>
-                      <AvatarImage src={session?.user?.image as string} />
-                      <AvatarFallback>AM</AvatarFallback>
-                    </Avatar>
-                    <h3 className="font-medium text-sm">
-                      {session?.user?.name}
-                    </h3>
-                  </div>
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="w-9 h-9"
-                    onClick={() => signIn("google")}
-                  >
-                    <LogOutIcon size={16} />
-                  </Button>
-                </div>
-
-                <div className="px-5 py-4">
-                  <Link href="/deliveries">
-                    <Button className="w-full" variant="outline">
-                      <ShoppingBagIcon className="mr-3" size={20} />
-                      Meus Pedidos
-                    </Button>
-                  </Link>
-                </div>
+                <MenuContent />
               </SheetContent>
             </Sheet>
           </div>
---
+++ app/_components/menu-content.tsx
@@ -0,0 +1,81 @@
+"use client";
+import {
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  ShoppingBagIcon,
+  ShoppingCartIcon,
+  UserCircle2Icon,
+} from "lucide-react";
+
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../_components/ui/sheet";
+
+import { Avatar, AvatarFallback, AvatarImage } from "../_components/ui/avatar";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function MenuContent() {
+  const { data: session } = useSession();
+  return (
+    <div>
+      <SheetHeader>
+        <SheetTitle className="text-left px-5 py-6 text-2xsl">Menu</SheetTitle>
+      </SheetHeader>
+      <div className="flex items-center justify-between px-6 py-4">
+        <div className="flex items-center gap-3">
+          <Avatar>
+            {session?.user ? (
+              <AvatarImage src={session?.user?.image ?? undefined} />
+            ) : (
+              <div className="flex items-center">
+                <UserCircle2Icon size={30} />
+              </div>
+            )}
+          </Avatar>
+          <h3 className="font-medium text-sm">
+            {session?.user ? session.user?.name : "Faça seu Login!"}
+          </h3>
+        </div>
+        {session?.user && (
+          <Button
+            variant="outline"
+            size="icon"
+            className="w-9 h-9"
+            onClick={() => signOut()}
+          >
+            <LogOutIcon size={16} />
+          </Button>
+        )}
+      </div>
+
+      <div className="px-5 py-4">
+        {session?.user ? (
+          <Link href="/deliveries">
+            <Button className="w-full" variant="outline">
+              <ShoppingBagIcon className="mr-3" size={20} />
+              Meus Pedidos
+            </Button>
+          </Link>
+        ) : (
+          <Button
+            className="w-full"
+            variant="outline"
+            onClick={() => signIn("google")}
+          >
+            <LogInIcon className="mr-3" size={20} />
+            Fazer Login
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
