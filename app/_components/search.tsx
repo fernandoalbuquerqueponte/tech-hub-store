1460b245fdc67a19a48b003da122a3979db8558c
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2024-09-24 15:10:47 -0300

    feat: add react-hook-form to search

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,32 +1,69 @@
 "use client";
 import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  search: z
+    .string()
+    .min(2, {
+      message: "O campo deve conter pelo menos 2 letras",
+    })
+    .trim(),
+});
 
 export default function Search() {
-  const [search, setSearch] = useState("");
   const router = useRouter();
-  function handleSearchProduct() {
-    router.push(`/product?search=${search}`);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function handleSubmit(data: z.infer<typeof FormSchema>) {
+    router.push(`/product?search=${data.search}`);
   }
   return (
-    <div className="flex flex-rol gap-3 items-center">
-      <Input
-        className="text-xs"
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Pesquisar Produto..."
-      />
-      <Button
-        className="min-w-11 min-h-10"
-        variant="outline"
-        size="icon"
-        onClick={handleSearchProduct}
-      >
-        <SearchIcon size={19} />
-      </Button>
+    <div className="flex items-center">
+      <Form {...form}>
+        <form
+          className="flex w-full gap-4"
+          onSubmit={form.handleSubmit(handleSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    className="text-xs"
+                    placeholder="Pesquisar Produto..."
+                    autoComplete="off"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            className="min-w-11 min-h-10"
+            variant="outline"
+            size="icon"
+            type="submit"
+          >
+            <SearchIcon size={19} />
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
