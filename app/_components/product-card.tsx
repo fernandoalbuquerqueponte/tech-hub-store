41e6470beb8cf271e50b3a3c3fe55ea9aa1753fe
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2024-10-31 21:28:53 -0300

    style: hide buy button on Product card

--- app/_components/product-card.tsx
+++ app/_components/product-card.tsx
@@ -3,7 +3,6 @@ import { Card, CardContent } from "./ui/card";
 import { ProductWithTotalPrice } from "../_helpers/product-price";
 import Link from "next/link";
 import DiscountBadgde from "./discount-badge";
-import { Button } from "./ui/button";
 
 interface ProductCardProps {
   product: ProductWithTotalPrice;
@@ -11,51 +10,53 @@ interface ProductCardProps {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <Card className="w-full">
-      <CardContent className="px-2 py-2 pb-5">
-        <div className="flex flex-col gap-1">
-          <div className="relative flex bg-zinc-900 w-full h-[135px] rounded-md justify-center items-center">
-            {product.discountPercentage > 0 && (
-              <div className="absolute top-1 left-2">
-                <DiscountBadgde
-                  discountPercentage={product.discountPercentage}
-                />
-              </div>
-            )}
-            <Image
-              width={100}
-              height={100}
-              className="max-h-[100px] max-w-[100px]"
-              src={product.imageUrl}
-              alt={product.name}
-            />
-          </div>
-          <h1 className="text-base font-medium text-ellipsis overflow-hidden text-nowrap">
-            {product.name}
-          </h1>
+    <Link href={`/product/${product.slug}`}>
+      <Card className="w-full">
+        <CardContent className="px-2 py-2 pb-5">
+          <div className="flex flex-col gap-1">
+            <div className="relative flex bg-zinc-900 w-full h-[135px] rounded-md justify-center items-center">
+              {product.discountPercentage > 0 && (
+                <div className="absolute top-1 left-2">
+                  <DiscountBadgde
+                    discountPercentage={product.discountPercentage}
+                  />
+                </div>
+              )}
+              <Image
+                width={100}
+                height={100}
+                className="max-h-[100px] max-w-[100px]"
+                src={product.imageUrl}
+                style={{
+                  objectFit: "cover",
+                }}
+                alt={product.name}
+              />
+            </div>
+            <h1 className="text-base font-medium text-ellipsis overflow-hidden text-nowrap">
+              {product.name}
+            </h1>
 
-          <div>
-            {product.discountPercentage > 0 ? (
-              <div className="text-ellipsis overflow-hidden text-nowrap w-full flex flex-col gap-1">
-                <h2 className="text-base font-bold">
-                  R$ {product.totalPrice.toFixed(2)}
-                </h2>
-                <span className="text-xs line-through text-[#c4c4c4]">
-                  R$ {Number(product.basePrice).toFixed(2)}
-                </span>
-              </div>
-            ) : (
-              <div className="text-ellipsis overflow-hidden text-nowrap w-full flex flex-row gap-2 items-center">
-                <h2 className="text-bold">R$ {product.totalPrice}</h2>
-              </div>
-            )}
-            <span className="text-xs text-[#c4c4c4]">Frete grátis</span>
-            <Link href={`/product/${product.slug}`}>
-              <Button className="w-full mt-2 bg-primary">Comprar</Button>
-            </Link>
+            <div>
+              {product.discountPercentage > 0 ? (
+                <div className="text-ellipsis overflow-hidden text-nowrap w-full flex flex-col gap-1">
+                  <h2 className="text-base font-bold">
+                    R$ {product.totalPrice.toFixed(2)}
+                  </h2>
+                  <span className="text-xs line-through text-[#c4c4c4]">
+                    R$ {Number(product.basePrice).toFixed(2)}
+                  </span>
+                </div>
+              ) : (
+                <div className="text-ellipsis overflow-hidden text-nowrap w-full flex flex-row gap-2 items-center">
+                  <h2 className="text-bold">R$ {product.totalPrice}</h2>
+                </div>
+              )}
+              <span className="text-xs text-[#c4c4c4]">Frete grátis</span>
+            </div>
           </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
